eb7a4cedb328b417b96ea8595286abad2a2ea537
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-25 01:26:45 +0900

    search for artists

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ extern crate rspotify;
     
 //     println!("# {} Top Tracks", result.tracks[0].artists[0].name);
 //     for track in result.tracks.iter() {
-//         println!("{0}: {1}", track.name, track.popularity);
+//         println!("{}: {}", track.name, track.popularity);
 //     }
 // }
 
@@ -32,7 +32,38 @@ async fn main() {
     let mut oauth = SpotifyOAuth::default().scope("user-read-private").build();
     match get_token(&mut oauth).await {
         Some(token_info) => {
-            println!("{:?}", token_info);
+            let client_credential = SpotifyClientCredentials::default()
+                .token_info(token_info)
+                .build();
+            let spotify = Spotify::default()
+                .client_credentials_manager(client_credential)
+                .build();
+            // Reference: https://developer.spotify.com/documentation/web-api/reference/search/search/
+            let result = spotify
+                .search(
+                    // q: 検索キーワード
+                    "DA PUMP",
+                    // type: 検索タイプ（album, artist, playlist, track, etc.）
+                    SearchType::Artist,
+                    // limit: 取得するデータの最大数
+                    1,
+                    // offset: 取得するデータの開始位置
+                    0,
+                    // market: 指定した国名コードで再生可能なコンテンツのみを返す
+                    Some(Country::Japan),
+                    // include_external: `audio` を指定すると関連するオーディオコンテンツがレスポンスに含まれる
+                    None,
+                )
+                .await
+                .unwrap();
+
+            match result {
+                rspotify::model::search::SearchResult::Artists(artists) => {
+                    let artist = &artists.items[0];
+                    println!("artist: {}\nid: {}\npopularity: {}", artist.name, artist.id, artist.popularity);
+                },
+                err => println!("search error!{:?}", err),
+            }
         }
         None => println!("auth failed"),
     };
