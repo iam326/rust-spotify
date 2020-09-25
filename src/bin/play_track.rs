3cfcfc9d9f202daa62fc7a79a4c41b1279aed162
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-25 11:49:58 +0900

    add play track bin

---
+++ src/bin/play_track.rs
@@ -0,0 +1,56 @@
+extern crate rspotify;
+
+use rspotify::client::Spotify;
+use rspotify::model::offset::for_position;
+use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
+use rspotify::util::get_token;
+
+#[tokio::main]
+async fn main() {
+    let mut oauth = SpotifyOAuth::default()
+        .scope("user-modify-playback-state,user-read-playback-state")
+        .build();
+
+    match get_token(&mut oauth).await {
+        Some(token_info) => {
+            let client_credential = SpotifyClientCredentials::default()
+                .token_info(token_info)
+                .build();
+            let spotify = Spotify::default()
+                .client_credentials_manager(client_credential)
+                .build();
+
+            // Reference: https://developer.spotify.com/documentation/web-api/reference/player/get-a-users-available-devices/
+            let result = spotify.device().await.unwrap();
+            let mut device_id = "";
+            for device in result.devices.iter() {
+                if let rspotify::senum::DeviceType::Computer = device._type {
+                    if device.is_active == true {
+                        device_id = &device.id;
+                    }
+                }
+            }
+
+            // Reference: https://developer.spotify.com/documentation/web-api/reference/player/start-a-users-playback/
+            match spotify
+                .start_playback(
+                    // device_id: 対象とするデバイスのID
+                    Some(device_id.to_string()),
+                    // context_uri: 再生する album, artist, playlist の指定
+                    None,
+                    // uris: 再生する track のリストを指定
+                    Some(vec!["spotify:track:623pmkD6sclgLBQrrPqyz4".to_owned()]),
+                    // offset: album, playlist, track の再生を開始する位置
+                    for_position(0),
+                    // position_ms: track の再生を開始する位置
+                    None
+                )
+                .await
+            {
+                Ok(_) => println!("start playback successful"),
+                Err(_) => eprintln!("start playback failed"),
+            }
+        }
+        None => println!("auth failed"),
+    }
+}
